1bdccce6be46379d899941f818429b32a32908ed
Calvert <wongcalvert@gmail.com>
2019-04-05 11:44:44 -0400

    added comment to superKeyword

--- src/superKeyword/SuperKeyword.java
+++ src/superKeyword/SuperKeyword.java
@@ -13,11 +13,17 @@ class Dog extends Animal {
 	
 	void printColor() {
 		System.out.println(color);
+		/**
+		 * Get the Animal's color which is white
+		 */
 		System.out.println(super.color);
 	}
 	
 	void sleep() {
 		System.out.println("sleeping until midnight");
+		/**
+		 * Call the sleep() in Animal class
+		 */
 		super.sleep();
 	}
 }
